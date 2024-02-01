bc9d9f83765340b4c471b709b5b1502978cc8adb
Alex62510 <eagle625@tut.by>
2024-02-01 10:14:43 +0300

    create error utils

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const app = express()
 // })
 //
 app.use(express.json())
-// app.use(cookieParser())
+app.use(cookieParser())
 app.listen(3000, () => {
     console.log('server run on port 3000')
 })
@@ -32,12 +32,12 @@ app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
 
 
-// app.use((err, req, res, next) => {
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || 'Internal server error';
-//     return res.status(statusCode).json({
-//         success: false,
-//         message,
-//         statusCode
-//     })
-// })
+app.use((err, req, res) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
