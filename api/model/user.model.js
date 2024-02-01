6246ab3cb6102fbaa08c88434d39d3b642ee5a3b
Alex62510 <eagle625@tut.by>
2024-02-01 16:48:52 +0300

    create listing

--- api/model/user.model.js
+++ api/model/user.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         require: true,
     },
-    avatar: {
+    profilePicture: {
         type: String,
         default: "https://t4.ftcdn.net/jpg/02/29/75/83/240_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
     }
