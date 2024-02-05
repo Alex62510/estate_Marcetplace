c1b9c786ef530b90717f20fd715a3a72916e21a2
Alex62510 <eagle625@tut.by>
2024-02-05 16:47:35 +0300

    create show listings button

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -14,6 +14,26 @@ import {
 } from '../redux/user/userSlice';
 import { Link } from 'react-router-dom';
 
+interface Listing {
+  _id: string;
+  imageUrls: string[];
+  name: string;
+  description: string;
+  address: string;
+  regularPrice: number;
+  discountPrice: number;
+  bathrooms: number;
+  bedrooms: number;
+  furnished: boolean;
+  parking: boolean;
+  type: string;
+  offer: boolean;
+  userRef: string;
+  createdAt: number;
+  updatedAt: number;
+  __v: number;
+}
+
 interface FormData {
   profilePicture?: string;
 }
@@ -27,6 +47,9 @@ export const Profile = () => {
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState<FormData>({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [loadingListing, setLoadingListing] = useState(false);
+  const [userListings, setUserListings] = useState<Listing[]>([]);
 
   const handleSaveImage = (e: ChangeEvent<HTMLInputElement>) => {
     e.target.files && setImage(e.target.files[0]);
@@ -116,7 +139,28 @@ export const Profile = () => {
       console.log(e);
     }
   };
-
+  const handleShowListings = async () => {
+    try {
+      if (!currentUser) {
+        return;
+      }
+      setLoadingListing(true);
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        setLoadingListing(false);
+        return;
+      }
+      setUserListings(data);
+      setLoadingListing(false);
+    } catch (e) {
+      setShowListingsError(true);
+      setLoadingListing(false);
+    }
+  };
+  console.log(userListings);
   return (
     <div className={'p-3 max-w-lg mx-auto'}>
       <h1 className={'text-3xl font-semibold text-center my-7'}>Profile</h1>
@@ -206,8 +250,47 @@ export const Profile = () => {
       </div>
       <p className={'text-red-700 mt-5'}>{error && 'Something went wrong!'}</p>
       <p className={'text-green-700 mt-5'}>
-        {updateSuccess && 'User updated successfully'}
+        {updateSuccess && 'User is updated successfully'}
       </p>
+      <button
+        disabled={loadingListing}
+        onClick={handleShowListings}
+        className={'text-green-700 w-full '}
+      >
+        {loadingListing ? 'Upload listings...' : 'Show listings'}
+      </button>
+      <p>{showListingsError ? 'Error showing listings' : ''}</p>
+      {userListings && userListings.length > 0 && (
+        <div className={'flex flex-col gap-4'}>
+          <h1 className={'text-center mt-7 text-2xl font-semibold'}>Your listings</h1>
+          {userListings.map(listing => (
+            <div
+              key={listing._id}
+              className={'border rounded-lg p-3 flex justify-between items-center gap-4'}
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className={'h-20 w-20 object-contain'}
+                />
+              </Link>
+              <Link
+                to={`/listing/${listing._id}`}
+                className={
+                  'flex-1 text-slate-700 font-semibold  hover:underline truncate'
+                }
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className={'flex flex-col items-center'}>
+                <button className={'text-red-700 uppercase'}>Delete</button>
+                <button className={'text-green-700 uppercase'}>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
